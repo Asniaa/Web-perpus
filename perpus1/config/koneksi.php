750955bc6923ff80143310b40b2948aca7702262
Asniaa <ameliaasnia@gmail.com>
2025-06-06 19:45:50 +0800

    Delete perpus1 directory

--- perpus1/config/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Menghubungkan database
-$db = mysqli_connect ('localhost','root','','db_perpus1');
-/*if (!$db) {
-    echo "Gagal Terhubung";
-} else {
-    echo "Selamat Anda Berhasil Terhubung";
-} */
-?>
